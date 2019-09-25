568c66788e0c61ee28fff45faa26a5ef8c99659b
Dmitriy Martynenko <dm.martyn1311@gmail.com>
2019-09-25 09:41:47 +0300

    refactor

--- src/main/java/eReportP24/TestRunner.java
+++ src/main/java/eReportP24/TestRunner.java
@@ -1,6 +1,7 @@
 package eReportP24;
 
 import eReportP24.entity.j3001001.J3001001DataItem;
+import eReportP24.entity.settingsData.SettingsDataItem;
 import eReportP24.pages.*;
 import eReportP24.pages.instructions.*;
 import eReportP24.utils.ConfigurationVariables;
@@ -11,6 +12,7 @@ import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
 import static eReportP24.utils.Utils.getJ3001001DataItem;
+import static eReportP24.utils.Utils.getNewSettingsDataItem;
 
 @Log4j
 @Epic("")
@@ -28,53 +30,53 @@ public class TestRunner extends SetUpAndTearDown {
     private SettingsPage settingsPage;
     private J3001001Page j3001001Page;
 
-//    @Test(description = "Проверим открытие станицы \"Сервіс «Електронна звітність»\"")
-//    public void checkElectronicReportingServicePage() {
-//        electronicReportingService = new MainPage().goToElectronicReportingService();
-//        electronicReportingService.checkElectronicReportServicePage();
-//    }
-//
-//    @Test(description = "Проверим открытие станицы \"Заява про приєднання до договору про визнання електронних документів\"")
-//    public void checkJoiningContractPage() {
-//        joiningContract = new MainPage().goToJoiningContract();
-//        joiningContract.checkJoiningContractPage();
-//    }
-//
-//    @Test(description = "Проверим открытие станицы \"Заявка на реєстрацію електронних цифрових підписів у системі електронної звітності органів державної статистики\"")
-//    public void checkApplicationForRegistrationPage() {
-//        applicationForRegistrationPage = new MainPage().goToApplicationForRegistrationPage();
-//        applicationForRegistrationPage.checkApplicationForRegistrationPage();
-//    }
-//
-//    @Test(description = "Проверим открытие станицы \"Інструкція підписання та надсилання електронної звітності за допомогою Crypto-Plugin\"")
-//    public void checkSigningAndSendingPagePage() {
-//        signingAndSending = new MainPage().goToSigningAndSendingPage();
-//        signingAndSending.checkSigningAndSendingPage();
-//    }
-//
-//    @Test(description = "Проверим открытие станицы \"Як отримати ПДВ-рахунок\"")
-//    public void checkVATInvoicePage() {
-//        vatInvoice = new MainPage().goToVATInvoicePage();
-//        vatInvoice.checkVATInvoicePage();
-//    }
-//
-//    @Test(description = "Проверим открытие станицы \"Інструкція звіту ЄСВ Додаток 4\"")
-//    public void checkReportAnnex4Page() {
-//        reportAnnex4 = new MainPage().goReportAnnex4Page();
-//        reportAnnex4.checkReportAnnex4Page();
-//    }
-//
-//    @Test(description = "Редактирование настроек")
-//    public void editSettings() {
-//        SettingsDataItem settingsDataItem = getNewSettingsDataItem();
-//        settingsPage = new MainPage().openSettings();
-//        settingsPage.editAllSettings(settingsDataItem);
-//        settingsPage.saveSettings();
-//        settingsPage.checkAllSettingsValues(settingsDataItem);
-//        settingsPage.closeSettingsPage();
-//        new MainPage().openSettings();
-//        settingsPage.checkAllSettingsValues(settingsDataItem);
-//    }
+    @Test(description = "Проверим открытие станицы \"Сервіс «Електронна звітність»\"")
+    public void checkElectronicReportingServicePage() {
+        electronicReportingService = new MainPage().goToElectronicReportingService();
+        electronicReportingService.checkElectronicReportServicePage();
+    }
+
+    @Test(description = "Проверим открытие станицы \"Заява про приєднання до договору про визнання електронних документів\"")
+    public void checkJoiningContractPage() {
+        joiningContract = new MainPage().goToJoiningContract();
+        joiningContract.checkJoiningContractPage();
+    }
+
+    @Test(description = "Проверим открытие станицы \"Заявка на реєстрацію електронних цифрових підписів у системі електронної звітності органів державної статистики\"")
+    public void checkApplicationForRegistrationPage() {
+        applicationForRegistrationPage = new MainPage().goToApplicationForRegistrationPage();
+        applicationForRegistrationPage.checkApplicationForRegistrationPage();
+    }
+
+    @Test(description = "Проверим открытие станицы \"Інструкція підписання та надсилання електронної звітності за допомогою Crypto-Plugin\"")
+    public void checkSigningAndSendingPagePage() {
+        signingAndSending = new MainPage().goToSigningAndSendingPage();
+        signingAndSending.checkSigningAndSendingPage();
+    }
+
+    @Test(description = "Проверим открытие станицы \"Як отримати ПДВ-рахунок\"")
+    public void checkVATInvoicePage() {
+        vatInvoice = new MainPage().goToVATInvoicePage();
+        vatInvoice.checkVATInvoicePage();
+    }
+
+    @Test(description = "Проверим открытие станицы \"Інструкція звіту ЄСВ Додаток 4\"")
+    public void checkReportAnnex4Page() {
+        reportAnnex4 = new MainPage().goReportAnnex4Page();
+        reportAnnex4.checkReportAnnex4Page();
+    }
+
+    @Test(description = "Редактирование настроек")
+    public void editSettings() {
+        SettingsDataItem settingsDataItem = getNewSettingsDataItem();
+        settingsPage = new MainPage().openSettings();
+        settingsPage.editAllSettings(settingsDataItem);
+        settingsPage.saveSettings();
+        settingsPage.checkAllSettingsValues(settingsDataItem);
+        settingsPage.closeSettingsPage();
+        new MainPage().openSettings();
+        settingsPage.checkAllSettingsValues(settingsDataItem);
+    }
 
     @Test(description = "Создадим документ")
     public void createDocument() {
@@ -90,17 +92,4 @@ public class TestRunner extends SetUpAndTearDown {
         singAndSendDocumentPage.uploadKeys();
     }
 
-    @Test(description = "Создадим документ")
-    public void createDocument2() {
-        j3001001Page = new J3001001Page();
-        J3001001DataItem j3001001DataItem = getJ3001001DataItem();
-        CreateDocument createDocument = new MainPage().goToCreateDocument();
-        createDocument.searchAndOpenDocumentTemplate(j3001001DataItem.getDeclarationName());
-        j3001001Page
-                .editDocument(j3001001DataItem)
-                .saveDocument()
-                .checkDocument(j3001001DataItem);
-        SingAndSendDocumentPage singAndSendDocumentPage = j3001001Page.singAndSendDocument();
-        singAndSendDocumentPage.uploadKeys();
-    }
 }
